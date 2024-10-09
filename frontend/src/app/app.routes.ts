b36ff22dc279cc05e385598f03325a21f9876e11
Kostya <kostikbushmelev@gmail.com>
2024-10-09 10:27:05 +0300

    сделал компонент авторизации

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+// import { RegistrationComponent } from './auth/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', component: HomeComponent }, // Главная страница
+  { path: 'login', component: LoginComponent }, // Страница авторизации
+  // { path: 'register', component: RegistrationComponent }, // Страница регистрации
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Перенаправление на главную, если путь не найден
+];
