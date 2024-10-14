2fe554e316aa191c0d185747ed28fe6a1282f2f9
Kostya <kostikbushmelev@gmail.com>
2024-10-14 14:45:38 +0300

    добавил всплывающие окна для выполнения функций

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -15,17 +15,21 @@ import {NgForOf, NgIf} from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  createFlag = 0
+  updateFlag = 0
+  deleteFlag = 0
+  searchFlag = 0
   title = 'Лабораторная работа'
   searchId: number | null = null;
 
   movie = {
     id: 1,
-    name: '',
+    name: 'Чип и Дейл',
     coordinates: { x: 0, y: 0 },
-    oscarsCount: 0,
-    budget: 0,
-    totalBoxOffice: 0,
-    mpaaRating: '',
+    oscarsCount: 3,
+    budget: 1000000000,
+    totalBoxOffice: 100000000000000,
+    mpaaRating: 'Самый крутой',
     director: { name: '', eyeColor: '', hairColor: '', location: { x: 0, y: 0, z: 0, name: '' }, height: 0, nationality: '' },
     screenwriter: { name: '', eyeColor: '', hairColor: '', location: { x: 0, y: 0, z: 0, name: '' }, height: 0, nationality: '' },
     operator: { name: '', eyeColor: '', hairColor: '', location: { x: 0, y: 0, z: 0, name: '' }, height: 0, nationality: '' },
@@ -35,27 +39,44 @@ export class HomeComponent {
     tagline: '',
     genre: ''
   };
+
   movies = [this.movie];
   constructor( private router: Router ) {}
 
-  navigateToLogin() {
+  login() {
     this.router.navigate(['/login'])
   }
 
-  createMovie() {
-    //TODO
+  home() {
+    this.router.navigate(['/home'])
   }
 
-  searchMovieById() {
-    //TODO
+  createMovie() {
+  }
+
+  changeCreateFlag() {
+    this.createFlag = this.createFlag ? 0 : 1
+  }
+
+  changeUpdateFlag() {
+    this.updateFlag = this.updateFlag ? 0 : 1
+  }
+
+  changeDeleteFlag() {
+    this.deleteFlag = this.deleteFlag ? 0 : 1
+  }
+
+  changeSearchFlag() {
+    this.searchFlag = this.searchFlag ? 0 : 1
+  }
+
+  searchMovie() {
   }
 
   updateMovie(id: number) {
-    //TODO
   }
 
   deleteMovie(id: number) {
-    //TODO
   }
 
   toDoMethod() {
